b2d03e2daa875a1f307e5f3b692afd3e74a7990a
Tomas Di Leo <39143171+TomasDiLeo@users.noreply.github.com>
2023-03-07 22:27:23 -0300

    Agregada la cabeza

--- overviewer.js
+++ overviewer.js
@@ -304,7 +304,7 @@ overviewer.util = {
         overviewer.map = L.map('mcmap', {crs: L.CRS.Simple});
 
         overviewer.map.attributionControl.setPrefix(
-            '<a href="https://overviewer.org">Overviewer/Leaflet</a>');
+            '<a href="/cabeza.html">Caja Cabeza</a>');
 
         overviewer.map.on('baselayerchange', function(ev) {
             
