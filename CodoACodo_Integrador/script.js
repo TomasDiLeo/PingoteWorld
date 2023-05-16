b6e19f342ee5916b0cca7184b43f18af5d5519f9
Tomas Di Leo <tomasjdle@gmail.com>
2023-05-16 15:01:30 -0300

    Update script.js

--- CodoACodo_Integrador/script.js
+++ CodoACodo_Integrador/script.js
@@ -1,10 +1,10 @@
 function calcularPrecio() {
-    var entradaPrecio = 100;
+    var entradaPrecio = 150;
     var cantidad = parseInt(document.getElementById("cantidad").value);
     
     if (cantidad && cantidad >= 1) {
         var precioTotal = entradaPrecio * cantidad;
-        document.getElementById("precioTotal").textContent = "El precio total es: " + precioTotal + " unidades monetarias.";
+        document.getElementById("precioTotal").textContent = "El precio total es: " + precioTotal + " pesos.";
     } else {
         document.getElementById("precioTotal").textContent = "La cantidad ingresada es inválida.";
     }
